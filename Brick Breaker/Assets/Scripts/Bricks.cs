f3bc289637167c74d50fef281d65683617b7f80c
Fx3xAxRx <fxexaxrx@gmail.com>
2019-02-03 20:51:33 +0530

    added brickbreaker proj to github

---
+++ Brick Breaker/Assets/Scripts/Bricks.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bricks : MonoBehaviour
+{
+    public GameObject brickParti;
+
+    void OnCollisionEnter(Collision other)
+    {
+        Instantiate(brickParti, transform.position, Quaternion.identity);
+        GM.instance.DestroyBrick();
+        Destroy(gameObject, 0.2f);
+        Ball.rb.AddForce(Ball.rb.velocity.normalized * Time.deltaTime * 30f);
+    }
+}
---
+++ Brick Breaker/Assets/Scripts/Padel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Padel : MonoBehaviour
+{
+    private Vector3 pdlPos = new Vector3();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pdlPos = transform.position;
+        float xPos = transform.position.x;
+        float zPos = transform.position.z;
+        pdlPos.x = Mathf.Clamp(xPos, -0.27f, 0.27f);
+        pdlPos.z = Mathf.Clamp(zPos, 0f, 0.17f);
+        transform.position = pdlPos;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        Ball.rb.AddForce(Ball.rb.velocity.normalized * Time.deltaTime * 30f);
+    }
+}
